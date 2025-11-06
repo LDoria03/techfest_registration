1da294dcbdc42b87a1389ff900e12dfc8f8c554e
Lorenzo Doria <lorenzoluis.doria@benilde.edu.ph>
2025-11-06 12:34:54 +0800

    Implemented participant registration

---
+++ techfest_registration.py
@@ -0,0 +1,12 @@
+# TechFest Registration System
+
+# Task 1: Registration Setup
+print("Welcome to SMIT TechFest!")
+print("Event organized by Lorenzo Luis F. Doria of APPDAET BTCS2")
+print()
+
+num_participants = int(input("How many participants will register? "))
+
+if num_participants <= 0:
+    print("Invalid number of participants.")
+
