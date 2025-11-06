24e62defdb3e0d7337eb3747fad8bfa9dc28db82
Lorenzo Doria <lorenzoluis.doria@benilde.edu.ph>
2025-11-06 12:36:22 +0800

    Added participant data collection and display

--- techfest_registration.py
+++ techfest_registration.py
@@ -9,4 +9,18 @@ num_participants = int(input("How many participants will register? "))
 
 if num_participants <= 0:
     print("Invalid number of participants.")
+else:
+    # Task 2: Collect Participant Information
+    participants = []
+
+    for i in range(num_participants):
+        name = input("Enter participant name: ")
+        track = input("Enter chosen track: ")
+        participant = {"name": name, "track": track}
+        participants.append(participant)
+
+    # Display registered participants
+    print("\nRegistered Participants:")
+    for i in range(len(participants)):
+        print(f"{i + 1}. {participants[i]['name']} - {participants[i]['track']}")
 
